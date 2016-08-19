3b0502059eb50c257045f8304fbf723080a92731
jixiang52002 <37310534@qq.com>
2016-08-19 17:36:56 +0800

    1,intent data 方式获取缩略图
    2.IO流获取原图

--- app/src/main/java/jixiang/com/mycamera/MainActivity.java
+++ app/src/main/java/jixiang/com/mycamera/MainActivity.java
@@ -21,9 +21,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    private static  final int REQ_1=1;
+    private static final int REQ_1 = 1;
 
-    private static  final int REQ_2=2;
+    private static final int REQ_2 = 2;
     private ImageView imageView;
 
     private ImageView imageView2;
@@ -33,48 +33,49 @@ public class MainActivity extends AppCompatActivity {
     FileInputStream fis;
 
     Bundle bundle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView= (ImageView) findViewById(R.id.iv_image);
-        imageView2= (ImageView) findViewById(R.id.iv_image2);
-        filePath= Environment.getExternalStorageDirectory().getPath();//获取sd卡的路径
-        filePath=filePath+"/"+"temp.png";
+        imageView = (ImageView) findViewById(R.id.iv_image);
+        imageView2 = (ImageView) findViewById(R.id.iv_image2);
+        filePath = Environment.getExternalStorageDirectory().getPath();//获取sd卡的路径
+        filePath = filePath + "/" + "temp.png";
     }
 
-    public void startCamera(View view){
-        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent,REQ_1);
+    public void startCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQ_1);
     }
 
 
-    public void startCamera2(View view){
-        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        Uri uri=Uri.fromFile(new File(filePath));
+    public void startCamera2(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri uri = Uri.fromFile(new File(filePath));
         //让拍照的照片放在指定的目录下 uri
-        intent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
-        startActivityForResult(intent,REQ_2);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivityForResult(intent, REQ_2);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK){
-            if(requestCode==REQ_1){
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQ_1) {
                 //由于原图过大，这里获取不到原图，仅仅是缩略图
-                bundle=data.getExtras();
-                Bitmap bitmap= (Bitmap) bundle.get("data");
+                bundle = data.getExtras();
+                Bitmap bitmap = (Bitmap) bundle.get("data");
                 imageView.setImageBitmap(bitmap);
-            }else if(requestCode==REQ_2){
+            } else if (requestCode == REQ_2) {
                 //通过将拍照后的照片保存在指定的目录下，然后通过io流获取相应的图像数据
                 try {
-                    fis=new FileInputStream(filePath);
-                    Bitmap bitmap= BitmapFactory.decodeStream(fis);
+                    fis = new FileInputStream(filePath);
+                    Bitmap bitmap = BitmapFactory.decodeStream(fis);
                     imageView2.setImageBitmap(bitmap);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
-                }finally {
+                } finally {
                     try {
                         fis.close();
                     } catch (IOException e) {
@@ -104,11 +105,11 @@ public class MainActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         //竖屏方向
-        if(getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE){
-            Log.d("orientation","LANDSCAPE");
-        }else if(getResources().getConfiguration().orientation==Configuration.ORIENTATION_PORTRAIT){
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Log.d("orientation", "LANDSCAPE");
+        } else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
             //横屏方向
-            Log.d("orientation","PROTRAT");
+            Log.d("orientation", "PROTRAT");
         }
     }
 }
